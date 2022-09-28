baafbdeed77f9eb77f8e310b73e237b8a12b0dc9
Octavio Chagas Junior <octaviopcjunior@hotmail.com>
2022-09-28 18:01:04 -0300

    Importando reduce, context e provider

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,13 +1,17 @@
+// import React from 'react'
 import "./Welcome.css"
-import React from 'react'
+import { useContext } from "react" 
 import Quiz from "../image/quiz.svg"
+import { QuizContext } from "../context/quiz"
 
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext)
+  
   return (
     <div id='welcome'>
       <h2>BEM VINDO!!!</h2>
       <p>Clique em iniciar para começar o QUIZ</p>
-      <button>Iniciar</button>
+      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>Iniciar</button>
       <img src={Quiz} alt='Iniciar o quiz' />
     </div>
   )
